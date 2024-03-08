02a5b467e60c989e822357a74683ad0726c847c7
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-03-07 21:58:52 -0500

    Fixed the response header and actually sent the body.

--- src/http.c
+++ src/http.c
@@ -4,8 +4,8 @@
 
 #include "http.h"
 
-struct string serialize_http_response(struct http_response* response) {
-    struct string string = new_string("HTTP 1.1 ");
+struct string serialize_http_response(const struct http_response* response) {
+    struct string string = new_string("HTTP/1.1 ");
     string_append_literal(&string, response->status);
     string_append_literal(&string, "\r\nContent-Type: ");
     string_append_literal(&string, response->content_type);
@@ -14,6 +14,9 @@ struct string serialize_http_response(struct http_response* response) {
     char content_length_string[64] = {0};
     sprintf(content_length_string, "%zu", response->content_length);
     string_append_literal(&string, content_length_string);
+
+    string_append_literal(&string, "\r\n\r\n");
+    string_append_literal(&string, response->body);
 
     return string;
 }
