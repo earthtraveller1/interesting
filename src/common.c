9bed58d5d28029726f52fcbdb93b6abb7c7c4e16
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-03-07 21:26:08 -0500

    Added some basic utilities for handling strings.

---
+++ src/common.c
@@ -0,0 +1,40 @@
+#include <string.h>
+#include <stdlib.h>
+
+#include "common.h"
+
+struct string new_string(const char* p_literal) {
+    struct string string;
+    memset(&string, 0, sizeof(string));
+
+    string.length = strlen(p_literal) + 1;
+    string.capacity = string.length;
+    string.data = malloc(string.capacity);
+    memcpy(string.data, p_literal, string.length);
+
+    return string;
+}
+
+void string_append_char(struct string* p_string, char p_char) {
+    if (p_string->length == p_string->capacity) {
+        p_string->capacity *= 2;
+        p_string->data = realloc(p_string->data, p_string->capacity);
+    }
+
+    p_string->data[p_string->length] = p_char;
+    p_string->length += 1;
+}
+
+void string_append_literal(struct string* p_string, const char* p_literal) {
+    for (const char* c = p_literal; *c; c++) {
+        string_append_char(p_string, *c);
+    }
+}
+
+void string_concat(struct string* p_string, const struct string* p_other) {
+    string_append_literal(p_string, p_other->data);
+}
+
+void free_string(const struct string* p_string) {
+    free(p_string->data);
+}
--- src/common.h
+++ src/common.h
@@ -1,11 +1,29 @@
 #ifndef INCLUDED_COMMON_H
 #define INCLUDED_COMMON_H
 
+#include <stddef.h>
+
 enum error {
     ERROR_SUCCESS = 0,
     ERROR_SOCKET_CREATION_FAILED = 1,
     ERROR_SOCKET_BIND_FAILED = 2,
     ERROR_SOCKET_LISTEN_FAILED = 3
 };
+
+struct string {
+    char* data;
+    size_t length;
+    size_t capacity;
+};
+
+struct string new_string(const char* literal);
+
+void string_append_char(struct string* string, char c);
+
+void string_append_literal(struct string* string, const char* literal);
+
+void string_concat(struct string* string, const struct string* other);
+
+void free_string(const struct string* string);
 
 #endif
