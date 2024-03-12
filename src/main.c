017284a07ab7bb21d78096b60421c5d64eb8998b
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-03-12 15:34:19 -0400

    Created a basic static file server.

--- src/main.c
+++ src/main.c
@@ -11,7 +11,6 @@
 #include <config.h>
 
 #include "common.h"
-#include "baseserver.h"
 #include "http.h"
 
 #ifdef INTERESTING_BUILD_TESTS
@@ -20,63 +19,53 @@
 
 #define PORT 6969
 
-static struct baseserver global_server = {0};
+static struct http_server global_server;
 
-void interrupt_handler(int signal) {
+static void interrupt_handler(int signal) {
     (void)signal;
 
     fprintf(stderr, "Closing server...\n");
-    destroy_baseserver(global_server);
+    free_http_server(&global_server);
     exit(0);
 }
 
-void on_connection(int client_socket, void* user_pointer) {
-    fprintf(stderr, "Client connected: %d\n", client_socket);
+static struct http_response on_request(const struct http_request* request) {
+    struct http_response response = {0};
 
-    (void)user_pointer;
-
-    struct string request_string = {0};
-
-    size_t read_amount = 0;
-    char character;
-
-    do {
-        read_amount = recv(client_socket, &character, 1, 0);
-        string_append_char(&request_string, character);
-
-        if (string_ends_with(&request_string, "\r\n\r\n")) {
-            break;
-        }
-    } while (read_amount > 0);
-
-    if (read_amount < 0) {
-        fprintf(stderr, "ERROR: Failed to read from socket\n");
-        close(client_socket);
-        return;
+    struct string page_path = new_string("pages");
+    if (strcmp(request->path.data, "/") == 0) {
+        string_append_literal(&page_path, "/index.html");
+    } else {
+        string_concat(&page_path, &request->path);
     }
 
-    const struct http_request request = parse_http_request(request_string.data);
+    FILE* page_file = fopen(page_path.data, "r");
+    if (page_file == NULL) {
+        response.status = "500 Internal Server Error";
+        response.content_type = "text/plain";
+        response.content_length = 0;
 
-    struct string body = new_string("<!DOCTYPE html><html><head><title>Hello, World!</title></head><body><h1>");
-    string_concat(&body, &request.path);
-    string_append_literal(&body, "</h1><p>Hello, World!</p></body></html>");
+        fprintf(stderr, "[ERROR]: Failed to open %s\n", page_path.data);
 
-    const struct http_response response = {
-        .status = "200 OK",
-        .content_type = "text/html",
-        .content_length = body.length,
-        .body = body
-    };
+        return response;
+    }
 
-    const struct string response_string = serialize_http_response(&response);
-    send(client_socket, response_string.data, response_string.length, 0);
-    close(client_socket);
+    char character;
+    struct string page_content = {0};
 
-    free_http_request(&request);
+    do {
+        character = fgetc(page_file);
+        string_append_char(&page_content, character);
+    } while (character != EOF);
 
-    free_string(&body);
-    free_string(&request_string);
-    free_string(&response_string);
+    response.status = "200 OK";
+    response.content_type = "text/html";
+    response.content_length = page_content.length;
+    response.body = page_content;
+
+    free_string(&page_path);
+
+    return response;
 }
 
 int main(int argc, char** argv) {
@@ -85,15 +74,15 @@ int main(int argc, char** argv) {
         return run_tests();
     }
 #endif
-    const struct server_error server_error = create_baseserver(PORT, INADDR_ANY);
-    if (server_error.error != ERROR_SUCCESS) {
+    struct http_server_error http_server = create_http_server(PORT, 0);
+    if (http_server.error != ERROR_SUCCESS) {
         return 1;
     }
 
-    global_server = server_error.server;
-    global_server.on_connection = on_connection;
-
+    global_server = http_server.server;
+    global_server.on_request = on_request;
+    
     signal(SIGINT, interrupt_handler);
 
-    return run_baseserver(&global_server);
+    return run_http_server(&global_server);
 }
